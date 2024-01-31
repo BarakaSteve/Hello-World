3abbb3dd54b88f28da69337219bcffc54800bbb1
Steve Baraka <steve.karimi@students.jkuat.ac.ke>
2024-02-01 00:36:31 +0300

    First Commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("My name is Karimi Steve Baraka\n");
+    printf("My course is Civil Engineering\n");
+    printf("My favorite team is M80\n");
+    printf("My favorite meal is NYAMA CHOMA\n");
+    return 0;
+}
